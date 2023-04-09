299387988d727722d1d461b1fd03004f003324d8
RIshi K <rushikesh.khire@gmail.com>
2023-04-08 19:03:39 -0500

    Flask web app.

---
+++ mockdb.py
@@ -0,0 +1,6 @@
+from main import Book , Publication , db
+
+
+b1 = Book( 'Mikys Delivery Service' , 'William Dobelli' , 3.9 , 'ePub' , 'broom-145379.svg' , 123 , 100)
+b2 = Book( 'Atomic Habbits' , 'Rayan Holiday' , 4.5 , 'hardcover' , 'cat-122379.svg' , 233 , 100)
+
--- run.py
+++ run.py
@@ -1,6 +1,75 @@
 from flask import Flask, request , render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+
+
+app.config.update(
+    SECERET_KEY='rishi123',
+    SQLALCHEMY_DATABASE_URI='postgresql://postgres:rishi123@localhost/catalogDB',
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
+)
+
+db = SQLAlchemy(app)
+
+class Publication(db.Model):
+    __tablename__ = 'publications'
+
+    id = db.Column(db.Integer , primary_key=True)
+    name = db.Column(db.String(80) , nullable=False)
+
+    def __init__(self , id , name):
+        self.id = id
+        self.name = name
+
+
+    def __repr__(self):
+        return 'The id is {} , name {} '.format(self.id , self.name)
+    
+
+
+
+
+class Book(db.Model):
+    __tablename__='book'
+
+    id = db.Column(db.Integer , primary_key=True)
+    title = db.Column(db.String(500) , nullable=False, index=True)
+    author = db.Column(db.String(450))
+    avg_rating = db.Column(db.Float)
+    format = db.Column(db.String(50))
+    image = db.Column(db.String(100), unique=True)
+    num_pages = db.Column(db.Integer)
+    pub_date = db.Column(db.DateTime, default=datetime.utcnow())
+
+    #relationship
+    pub_id = db.Column(db.Integer , db.ForeignKey('publications.id'))
+
+    def __init(self, title , author, avg_rating , book_format , image , num_pages ,pub_id):
+
+        self.title = title
+        self.author =author
+        self.avg_rating = avg_rating
+        self.format = book_format
+        self.image = image
+        self.num_pages = num_pages
+        self.pub_id = pub_id
+
+        def __repr__(sefl):
+            return '{} by {}'.format(self.title , self.author)
+        
+
+
+
+@app.before_first_request
+def create_database():
+     db.create_all()
+
+
+
+
+
 
 
 @app.route('/')
@@ -89,5 +158,6 @@ def jinja_macros():
     return render_template('using_macros.html' , movies=movies_dict)
 
 if __name__ == '__main__':
+
     app.run(debug=True)
     
